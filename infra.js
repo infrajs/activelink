849d4ed7e6c655e862219231fff6da6a8276f83c
Антон Кияткин <aky@list.ru>
2015-11-24 00:20:13 +0400

    comments

--- infra.js
+++ infra.js
@@ -1,10 +1,11 @@
-infra.listen(infra, 'layer.onshow', function(layer){
-    if (!layer.div||!layer.activelink) return;
+infra.listen(infra, 'layer.onshow', function (layer) {
+    if (!layer.div || !layer.activelink) return;
     delete layer.activelink;
-    infra.handle(infra.Crumb,'onchange',function(){
-        infra.wait(infrajs,'onshow',function(){
-            if(!layer.showed)return;
-            activelink($('#'+layer.div));
+    infra.handle(infra.Crumb, 'onchange', function () { //Чтобы при любом onchange и хотябы один onshow уже был
+    	//Функция выполняется при вперёд, назад
+        infra.wait(infrajs, 'onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
+            if (!layer.showed) return;//Если в текущем окружении слой скрыт, ждём.
+            activelink('#' + layer.div);
         });
     });
 });
