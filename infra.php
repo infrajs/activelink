d36daff48d210d4aa31c6d11f8ab369c019760d9
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-16 01:06:40 +0400

    * removed. ~,-

--- infra.php
+++ infra.php
@@ -4,6 +4,6 @@ use infrajs\event\Event;
 use infrajs\view\View;
 
 Event::handler('onjs', function () {
-	View::js('*activelink/activelink.js');
-	View::js('*activelink/infra.js');
+	View::js('-activelink/activelink.js');
+	View::js('-activelink/infra.js');
 });
