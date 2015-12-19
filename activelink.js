6537999b7ebb8ad41d3647a1578c38b011d82de4
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-19 13:59:41 +0400

    infra config

--- activelink.js
+++ activelink.js
@@ -1,13 +1,16 @@
-window.activelink=function(div){
-    var href=location.href;
-    if (!div) div=document;
-    $(div).find('a').removeClass('active').each(function(){
-        var a=$(this);
-        if (a.prop('href')==href) {
-            a.addClass('active');
-            a.parent().filter('li').addClass('active');
-        } else {
-            a.removeClass('active').parent().filter('li').removeClass('active');
-        }
-    });
-}
+define('?-activelink/activelink.js', function () {
+    window.activelink=function(div){
+        var href=location.href;
+        if (!div) div=document;
+        $(div).find('a').removeClass('active').each(function(){
+            var a=$(this);
+            if (a.prop('href')==href) {
+                a.addClass('active');
+                a.parent().filter('li').addClass('active');
+            } else {
+                a.removeClass('active').parent().filter('li').removeClass('active');
+            }
+        });
+    }
+    return window.activelink;
+});
--- infra.js
+++ infra.js
@@ -1,11 +1,13 @@
-infra.listen(infra, 'layer.onshow', function (layer) {
-    if (!layer.div || !layer.activelink) return;
-    delete layer.activelink;
-    infra.handle(infra.Crumb, 'onchange', function () { //Чтобы при любом onchange и хотябы один onshow уже был
-    	//Функция выполняется при вперёд, назад
-        infra.wait(infrajs, 'onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
-            if (!layer.showed) return;//Если в текущем окружении слой скрыт, ждём.
-            activelink('#' + layer.div);
-        });
-    });
+define('?-activelink/infra.js', ['?-event/event.js','?-activelink/activelink.js'], function () {
+	infra.listen(infra, 'layer.onshow', function (layer) {
+	    if (!layer.div || !layer.activelink) return;
+	    delete layer.activelink;
+	    infra.handle(infra.Crumb, 'onchange', function () { //Чтобы при любом onchange и хотябы один onshow уже был
+	    	//Функция выполняется при вперёд, назад
+	        infra.wait(infrajs, 'onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
+	            if (!layer.showed) return;//Если в текущем окружении слой скрыт, ждём.
+	            activelink('#' + layer.div);
+	        });
+	    });
+	});
 });
