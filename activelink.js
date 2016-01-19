deb26d3dc4db0b4da6ac884824132495b8d56b63
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-01-19 06:46:06 +0400

    fix for uley

--- activelink.js
+++ activelink.js
@@ -1,16 +1,32 @@
-define('?-activelink/activelink.js', function () {
-    window.activelink=function(div){
-        var href=location.href;
-        if (!div) div=document;
-        $(div).find('a').removeClass('active').each(function(){
+(function () {
+	window.activelink=function(div){
+		var href=location.href;
+		
+		if (!div) div=document;
+		var is=false;
+		$(div).find('a').removeClass('active').each(function(){
+			var a=$(this);
+			if (a.prop('href')==href) {
+				a.addClass('active');
+				a.parent().filter('li').addClass('active');
+				is=true;
+			} else {
+				a.removeClass('active').parent().filter('li').removeClass('active');
+			}
+		});
+		if(is)return;
+		var r=href.split('#');
+		href=r[0];
+		$(div).find('a').removeClass('active').each(function(){
             var a=$(this);
             if (a.prop('href')==href) {
                 a.addClass('active');
                 a.parent().filter('li').addClass('active');
+                is=true;
             } else {
                 a.removeClass('active').parent().filter('li').removeClass('active');
             }
         });
-    }
-    return window.activelink;
-});
+	}
+	return window.activelink;
+})();
