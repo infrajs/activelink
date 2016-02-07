5af043fe4a51badfc1249ce520babca8d0bf95a7
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-02-07 19:34:49 +0400

    fix for uley

--- infra.js
+++ infra.js
@@ -2,8 +2,10 @@
 	Event.handler('layer.onshow', function (layer) {
 	    if (!layer.div || !layer.activelink) return;
 	    delete layer.activelink;
+
 	    Event.handler('Crumb.onchange', function () { //Чтобы при любом onchange и хотябы один onshow уже был
 	    	//Функция выполняется при вперёд, назад
+
 	        Event.one('Infrajs.onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
 	            if (!layer.showed) return;//Если в текущем окружении слой скрыт, ждём.
 	            activelink('#' + layer.div);
