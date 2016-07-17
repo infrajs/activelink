80d796f9456e712f1ae57917f6147d8d5647f25a
Anton <a.kiyatkin@itlf.ru>
2016-07-17 12:17:52 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,6 +1,6 @@
 (function () {
 	Event.handler('layer.onshow', function (layer) {
-	    if (!layer.div || !layer.activelink) return;
+	    if (!layer.activelink) return;
 	    delete layer.activelink;
 
 	    Event.handler('Crumb.onchange', function () { //Чтобы при любом onchange и хотябы один onshow уже был
@@ -8,7 +8,9 @@
 
 	        Event.one('Infrajs.onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
 	            if (!layer.showed) return;//Если в текущем окружении слой скрыт, ждём.
-	            activelink('#' + layer.div);
+
+	            if (layer.div) activelink('#' + layer.div);
+	            else activelink();
 	        });
 	    });
 	});
