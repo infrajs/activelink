8a697486662d77905da92a0aad191d7e0acee878
Anton <a.kiyatkin@itlf.ru>
2017-04-25 18:32:23 +0400

    dealers

--- infra.js
+++ infra.js
@@ -1,12 +1,12 @@
 (function () {
-	Event.handler('layer.onshow', function (layer) {
+	Event.handler('Layer.onshow', function (layer) {
 	    if (!layer.activelink) return;
 	    delete layer.activelink;
 
 	    Event.handler('Crumb.onchange', function () { //Чтобы при любом onchange и хотябы один onshow уже был
 	    	//Функция выполняется при вперёд, назад
 
-	        Event.one('Infrajs.onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
+	        Event.one('Controller.onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
 	            if (!layer.showed) return;//Если в текущем окружении слой скрыт, ждём.
 
 	            if (layer.div) activelink('#' + layer.div);
