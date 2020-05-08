ab21c0b4b968625cc310a5091b05d4cec49375a0
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-08 17:55:53 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,17 +1,14 @@
-(function () {
-	Event.handler('Layer.onshow', function (layer) {
-	    if (!layer.activelink) return;
-	    delete layer.activelink;
+import { Event } from '/vendor/infrajs/event/Event.js'
 
-	    Event.handler('Crumb.onchange', function () { //Чтобы при любом onchange и хотябы один onshow уже был
-	    	//Функция выполняется при вперёд, назад
-
-	        Event.one('Controller.onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
-	            if (!layer.showed) return;//Если в текущем окружении слой скрыт, ждём.
-
-	            if (layer.div) activelink('#' + layer.div);
-	            else activelink();
-	        });
-	    });
+Event.handler('Layer.onshow', layer => {
+	if (!layer.activelink) return;
+	delete layer.activelink;
+	Event.handler('Crumb.onchange', function () { //Чтобы при любом onchange и хотябы один onshow уже был
+		//Функция выполняется при вперёд, назад
+		Event.one('Controller.onshow', function () {//Если это первый запуск то нужно дождаться Onshow infrajs чтобы изменения слоя применились
+			if (!layer.showed) return;//Если в текущем окружении слой скрыт, ждём.
+			if (layer.div) activelink('#' + layer.div);
+			else activelink();
+		});
 	});
-})();
+});
